8674c0f49747a4f5990d84b33168833e050d5e7e
Lucas Duarte <lucasduartechaves@hotmail.com>
2024-09-02 23:00:45 -0300

    Adiciona autenticação de usuário

--- src/main/java/br/ufjf/ffapi/service/UsuarioService.java
+++ src/main/java/br/ufjf/ffapi/service/UsuarioService.java
@@ -1,8 +1,16 @@
+
 package br.ufjf.ffapi.service;
 
 import br.ufjf.ffapi.exception.RegraNegocioException;
+import br.ufjf.ffapi.exception.SenhaInvalidaException;
 import br.ufjf.ffapi.model.entity.Usuario;
 import br.ufjf.ffapi.model.repository.UsuarioRepository;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,19 +19,21 @@ import java.util.Objects;
 import java.util.Optional;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
+
+    private PasswordEncoder encoder;
 
     private UsuarioRepository repository;
 
-    public UsuarioService(UsuarioRepository repository){
+    public UsuarioService(UsuarioRepository repository) {
         this.repository = repository;
     }
 
-    public List<Usuario> getUsuarios(){
+    public List<Usuario> getUsuarios() {
         return repository.findAll();
     }
 
-    public Optional<Usuario> getUsuarioById(Long id){
+    public Optional<Usuario> getUsuarioById(Long id) {
         return repository.findById(id);
     }
 
@@ -31,6 +41,34 @@ public class UsuarioService {
     public Usuario salvar(Usuario usuario) {
         validar(usuario);
         return repository.save(usuario);
+    }
+
+    public UserDetails autenticar(Usuario usuario) {
+        UserDetails user = loadUserByUsername(usuario.getLogin());
+        boolean senhasBatem = encoder.matches(usuario.getSenha(), user.getPassword());
+
+        if (senhasBatem) {
+            return user;
+        }
+        throw new SenhaInvalidaException();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = repository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        String[] roles = usuario.isAdmin()
+                ? new String[] { "ADMIN", "USER" }
+                : new String[] { "USER" };
+
+        return User
+                .builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
+                .build();
     }
 
     @Transactional
@@ -45,12 +83,6 @@ public class UsuarioService {
         }
         if (usuario.getCpf() == null || usuario.getCpf().trim().equals("")) {
             throw new RegraNegocioException("CPF inválido");
-        }
-        if (usuario.getSenha() == null || usuario.getSenha().trim().equals("")) {
-            throw new RegraNegocioException("Senha inválida");
-        }
-        if (!usuario.isAdmin()) {
-            throw new RegraNegocioException("Admin inválido");
         }
     }
 }
