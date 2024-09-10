f1fc94cd3b88d548a76a7951bcfa11d493b2568f
Rodrigo Soares <rodrigo.assis@estudante.ufjf.br>
2024-09-10 18:38:28 -0300

    checkpoint 7

--- src/main/java/br/ufjf/ffapi/api/dto/UsuarioDTO.java
+++ src/main/java/br/ufjf/ffapi/api/dto/UsuarioDTO.java
@@ -15,7 +15,7 @@ public class UsuarioDTO {
     private String login;
     private String cpf;
     private String senha;
-    private String repeticaoSenha;
+//    private String repeticaoSenha;
     private boolean admin;
 
     public static UsuarioDTO create(Usuario usuario){
